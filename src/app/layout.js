0380a7973025f306411b5995e8de31a5ca534976
Arjun Prajapati <arjunprajapati4102001@gmail.com>
2026-05-26 11:38:22 +0530

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Signature Slam Academy(SSA)",
+  description: "Adopt, Nurture, Deliver",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
